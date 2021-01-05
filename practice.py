5fc75dbb19f47d344bc34f66e313908cbb6b8d2f
GermanMan <adammstrotman@gmail.com>
2021-01-05 10:49:16 -0500

    Creates website that counts words and how many of each word. 3 pages

---
+++ practice.py
@@ -0,0 +1,12 @@
+'''class Car:
+    def __init__(self, yearmade, currentyear, age):
+        self.yearmade = yearmade
+        self.currentyear = currentyear
+        self.age = currentyear - yearmade
+
+carAge = Car(2015, 2020, 0)
+
+print(carAge.age)'''
+'''
+to opena  new django file you must type 
+python -m django startproject name
